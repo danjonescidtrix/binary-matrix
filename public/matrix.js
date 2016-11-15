99d0396696f3f596e97bea44260b1e1e1ab2b495
danjonescidtrix <dannyjones360@me.com>
2016-11-15 11:39:15 +0000

    minor revisions
    Clean up and speed up of javascript, Removed certain colours.

--- public/matrix.js
+++ public/matrix.js
@@ -1,82 +1,76 @@
-$(document).ready(function () {
+$(function () {
 
-  //fade body in
-  $("body").fadeIn(300);
+    //fade body in
+    $("body").fadeIn(300);
 
-  //creates binary items inside matrix container
-  for (i = 0; i < 1500; i++) {
-    $('#binaryMatrix').append('<div id="' + i + '" class="binary">' + 0 + '</div>');
-  }
+    //creates binary items inside matrix container
+    for (i = 0; i < 1500; i++) {
+        $('#binaryMatrix').append('<div id="' + i + '" class="binary">' + 0 + '</div>');
+    }
 
-  //loops the binary items
-  $("#binaryMatrix .binary").each(function (i, obj) {
-    flipValue(obj.id);
-    flipValue2(obj.id);
-    flipValue3(obj.id);
-    flipValue4(obj.id);
-    flipValue5(obj.id);
-    flipValue6(obj.id);
-    flipValue7(obj.id);
-    flipValue8(obj.id);
-  });
+    //loops the binary items
+    $("#binaryMatrix .binary").each(function (i, obj) {
 
-  //binary int change func
-  function flipValue(id) {
-    setInterval(function () {
-      $('#' + id + '').text(function (i, t) {
-        return t == '1' ? '0' : '1';
-      });
-    }, Math.round(Math.random() * 10000) + 400); // random between 200 - 1000
-  };
 
-  //binary int change func
-  function flipValue2(id) {
-    setInterval(function () {
-      $('#' + id + '').css("font-weight", "bolder");
-    }, Math.round(Math.random() * 100000) + 400); // random between 200 - 1000
-  };
+        var randomList = ['bold', 'normal', 'black', 'purple']
+        var random = randomList[Math.floor(Math.random() * randomList.length)];
 
-  //binary int change func
-  function flipValue3(id) {
-    setInterval(function () {
-      $('#' + id + '').css("font-weight", "normal");
-    }, Math.round(Math.random() * 5000) + 400); // random between 200 - 1000
-  };
+        flipValue(obj.id);
 
-  //binary int change black
-  function flipValue4(id) {
-    setInterval(function () {
-      $('#' + id + '').css("color", "black");
-    }, Math.round(Math.random() * 10000) + 400); // random between 200 - 1000
-  };
+        switch (random) {
+            case "bold":
+                flipValue2(obj.id);
+                break;
+            case "normal":
+                flipValue3(obj.id);
+                break;
+            case "black":
+                flipValue4(obj.id);
+                break;
+            case "purple":
+                flipValue5(obj.id);
+                break;
+        }
 
-  //binary int change purple
-  function flipValue5(id) {
-    setInterval(function () {
-      $('#' + id + '').css("color", "purple");
-    }, Math.round(Math.random() * 120000) + 400); // random between 200 - 1000
-  };
+    });
 
-  //binary int change green
-  function flipValue6(id) {
-    setInterval(function () {
-      $('#' + id + '').css("color", "green");
-    }, Math.round(Math.random() * 120000) + 400); // random between 200 - 1000
-  };
+    //binary int change func
+    function flipValue(id) {
+        setInterval(function () {
+            $('#' + id + '').text(function (i, t) {
+                return t == '1' ? '0' : '1';
+            });
+        }, Math.round(Math.random() * 10000) + 200); // random between 200 - 1000
+    };
 
-  //binary int change blue
-  function flipValue7(id) {
-    setInterval(function () {
-      $('#' + id + '').css("color", "blue");
-    }, Math.round(Math.random() * 120000) + 400); // random between 200 - 1000
-  };
+    //binary int change func
+    function flipValue2(id) {
+        setInterval(function () {
+            $('#' + id + '').css("font-weight", "bolder");
+        }, Math.round(Math.random() * 5000) + 400); // random between 200 - 1000
+    };
 
-  //binary int change red
-  function flipValue8(id) {
-    setInterval(function () {
-      $('#' + id + '').css("color", "red");
-    }, Math.round(Math.random() * 120000) + 400); // random between 200 - 1000
-  };
+    //binary int change func
+    function flipValue3(id) {
+        setInterval(function () {
+            $('#' + id + '').css("font-weight", "normal");
+        }, Math.round(Math.random() * 5000) + 400); // random between 200 - 1000
+    };
+
+    //binary int change black
+    function flipValue4(id) {
+        setInterval(function () {
+            $('#' + id + '').css("color", "black");
+        }, Math.round(Math.random() * 5000) + 400); // random between 200 - 1000
+    };
+
+    //binary int change purple
+    function flipValue5(id) {
+        setInterval(function () {
+            $('#' + id + '').css("color", "purple");
+        }, Math.round(Math.random() * 5000) + 400); // random between 200 - 1000
+    };
+
 
 
 });
