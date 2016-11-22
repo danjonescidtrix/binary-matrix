ba6b4111bf8a24adec68fe8c674060d26b7aa174
danjonescidtrix <dannyjones360@me.com>
2016-11-22 12:52:12 +0000

    added colour palette
    Added a fixed colour palette with multile clicking options.

--- public/matrix.js
+++ public/matrix.js
@@ -1,11 +1,13 @@
 $(function () {
 
+    PALARRAY = ['binary', 'bold', 'normal', 'black', 'gray'];
+
     //fade body in
     $("body").fadeIn(300);
 
     //creates binary items inside matrix container
     for (i = 0; i < 189; i++) {
-        $('#binaryMatrix').append('<div id="' + i + '" class="binary">' + '0' + '</div>');
+        $('#binaryMatrix').append('<div id="binary-item-' + i + '" class="binary">' + '0' + '</div>');
     }
 
     //loops the binary items
@@ -13,8 +15,7 @@ $(function () {
 
         setInterval(function () {
 
-            var randomList = ['binary', 'bold', 'normal', 'black', 'blue', 'purple', 'gray'],
-                random = randomList[Math.floor(Math.random() * randomList.length)],
+            var random = PALARRAY[Math.floor(Math.random() * PALARRAY.length)],
                 item = $('#' + obj.id + '');
 
             switch (random) {
@@ -30,19 +31,62 @@ $(function () {
                 case "black":
                     item.css("color", "black");
                     break;
-                case "purple":
-                    item.css("color", "purple");
-                    break;
+                case "gray":
+                    item.css("color", "#575757");
+                    break
                 case "blue":
                     item.css("color", "#4F94CD");
                     break
-                case "gray":
-                    item.css("color", "#575757");
+                case "purple":
+                    item.css("color", "purple");
+                    break;
+                case "red":
+                    item.css("color", "red");
+                    break;
+                case "green":
+                    item.css("color", "green");
+                    break
+                case "orange":
+                    item.css("color", "darkorange");
+                    break;
+                case "yellow":
+                    item.css("color", "#e6e600");
                     break
             }
 
         }, Math.floor(Math.random() * (1000 - 200 + 1) + 200));
     });
 
+
+    //adds colours to array
+    $("#colour-pal .colour-pal--item").click(function () {
+        var palItem = $('#' + this.id + '');
+        switch (this.id) {
+            case "colour-pal-1":
+                PALARRAY.push("blue");
+                palItem.css("opacity", "1");
+                break
+            case "colour-pal-2":
+                PALARRAY.push("purple");
+                palItem.css("opacity", "1");
+                break
+            case "colour-pal-3":
+                PALARRAY.push("red");
+                palItem.css("opacity", "1");
+                break
+            case "colour-pal-4":
+                PALARRAY.push("green");
+                palItem.css("opacity", "1");
+                break
+            case "colour-pal-5":
+                PALARRAY.push("orange");
+                palItem.css("opacity", "1");
+                break
+            case "colour-pal-6":
+                PALARRAY.push("yellow");
+                palItem.css("opacity", "1");
+                break
+        }
+    });
 
 });
